702f9b123a86451fbc587f09aaba0b97c4090378
mhdkhubbi <mhd.khubbi@gmail.com>
2026-01-03 18:39:46 +0300

    adding view model package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.ui.text.google.fonts)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
