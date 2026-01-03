96228c6a60fa4e8f3066fc16bf491e4d6feb5588
mhdkhubbi <mhd.khubbi@gmail.com>
2026-01-03 14:33:38 +0300

    creating StorageInfo and CustomCircularProgressIndicator composes

--- app/src/main/java/io/mhdkhubbi/filo/ui/theme/components/Storage.kt
+++ app/src/main/java/io/mhdkhubbi/filo/ui/theme/components/Storage.kt
@@ -1,2 +1,64 @@
 package io.mhdkhubbi.filo.ui.theme.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun StorageInfo() {
+
+    Column(
+        modifier = Modifier.padding(8.dp), verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            CustomCircularProgressIndicator()
+            Column(verticalArrangement = Arrangement.Center,
+                ) {
+                Text("Internal Storage", color = Color(0xFF9CA3AF))
+                Spacer(Modifier.height(8.dp))
+                Text("85 GB of 128 GB Used")
+            }
+
+        }
+
+
+    }
+}
+@Composable
+fun CustomCircularProgressIndicator(){
+    Box(
+        modifier = Modifier
+
+            .background(Color(0xFFF9FAFB), RoundedCornerShape(12.dp)),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator({ 0.6f }, strokeWidth = 7.dp)
+        Text("66%", modifier = Modifier.padding(20.dp), fontSize = 10.sp)
+    }
+
+}
+@Composable
+@Preview
+fun StorageInfoPreview() {
+    StorageInfo()
+}
