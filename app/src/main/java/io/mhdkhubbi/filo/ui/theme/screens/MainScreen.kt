90f7a2827a52a6de026ceefdf34495039a809408
mhdkhubbi <mhd.khubbi@gmail.com>
2026-01-03 20:59:43 +0300

    Adding Settings icon to mainScreen

--- app/src/main/java/io/mhdkhubbi/filo/ui/theme/screens/MainScreen.kt
+++ app/src/main/java/io/mhdkhubbi/filo/ui/theme/screens/MainScreen.kt
@@ -1,10 +1,15 @@
 package io.mhdkhubbi.filo.ui.theme.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,6 +17,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import io.mhdkhubbi.filo.ui.theme.Gray500
+import io.mhdkhubbi.filo.ui.theme.components.FolderComose
 import io.mhdkhubbi.filo.ui.theme.components.SearchField
 import io.mhdkhubbi.filo.ui.theme.components.StorageInfo
 
@@ -26,7 +33,16 @@ fun MainScreen(modifier: Modifier = Modifier) {
         ) {
 
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("File Manager", fontSize = 30.sp, fontWeight = FontWeight.Medium)
+            Row(modifier = Modifier.fillMaxWidth().padding(start = 15.dp, end = 15.dp),
+                verticalAlignment = Alignment.CenterVertically){
+                Text(modifier=Modifier.weight(1f),
+                    text="File Manager", fontSize = 30.sp, fontWeight = FontWeight.Medium)
+                Icon(
+                    Icons.Default.Settings,
+                    contentDescription = "Search",
+                    tint = Gray500
+                )
+            }
             Spacer(Modifier.height(40.dp))
             SearchField()
         }
@@ -37,8 +53,9 @@ fun MainScreen(modifier: Modifier = Modifier) {
         Text(modifier = Modifier.padding(start = 10.dp, top = 10.dp),
             text = "Favorites",
             fontWeight = FontWeight.SemiBold,
-            fontSize = 20.sp,
+            fontSize = 21.sp,
         )
+        FolderComose()
 
     }
 }
