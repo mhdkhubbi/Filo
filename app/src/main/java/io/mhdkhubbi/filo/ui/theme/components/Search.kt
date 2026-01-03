71cec021b147d77d2df816fbe2b6dbc2afca3d24
mhdkhubbi <mhd.khubbi@gmail.com>
2026-01-03 14:41:42 +0300

    change  CenteredSearchField to SearchFiled

--- app/src/main/java/io/mhdkhubbi/filo/ui/theme/components/Search.kt
+++ app/src/main/java/io/mhdkhubbi/filo/ui/theme/components/Search.kt
@@ -28,7 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun CenteredSearchField(
+fun SearchField(
     values: String="",
     onValueChange: (String) -> Unit={}
 ) {
@@ -74,6 +74,6 @@ fun CenteredSearchField(
 @Preview
 @Composable
 fun SearchPreview() {
-    CenteredSearchField(
+    SearchField(
     )
 }
