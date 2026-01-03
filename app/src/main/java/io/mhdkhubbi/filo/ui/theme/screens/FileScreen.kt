1ee8a823e86340b5f13f5e57754ff8db4270db5d
mhdkhubbi <mhd.khubbi@gmail.com>
2026-01-03 21:25:19 +0300

    Add FileScreen component with multiple FolderComose instances

---
+++ app/src/main/java/io/mhdkhubbi/filo/ui/theme/screens/FileScreen.kt
@@ -0,0 +1,19 @@
+package io.mhdkhubbi.filo.ui.theme.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import io.mhdkhubbi.filo.ui.theme.components.FolderComose
+
+@Composable
+fun FileScreen(){
+
+    Column{
+        FolderComose()
+        FolderComose()
+        FolderComose()
+        FolderComose()
+        FolderComose()
+    }
+
+
+}
