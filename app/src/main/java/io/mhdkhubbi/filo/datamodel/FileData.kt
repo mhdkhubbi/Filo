37370b825b5792eea8fd342b45d902a0eafbd87b
mhdkhubbi <mhd.khubbi@gmail.com>
2026-01-04 19:45:25 +0300

    Add data model for file system entries and storage statistics

---
+++ app/src/main/java/io/mhdkhubbi/filo/datamodel/FileData.kt
@@ -0,0 +1,25 @@
+package io.mhdkhubbi.filo.datamodel
+
+data class FsEntry(
+    val name: String,
+    val fullPath: String,
+    val isDirectory: Boolean,
+    val type: FileType,
+    val sizeBytes: Long,
+    val itemCount: Int
+)
+
+
+enum class FileType {
+    FOLDER,
+    PDF,
+    APK,
+    IMAGE,
+    VIDEO,
+    AUDIO,
+    OTHER
+}
+data class StorageStats(
+    val usedBytes: Long,
+    val totalBytes: Long
+)
