eedb125155f2aaa35dc7f5c328a75a1ec4550f49
mhdkhubbi <mhd.khubbi@gmail.com>
2026-01-06 11:57:18 +0300

    feat: Display current directory name

--- app/src/main/java/io/mhdkhubbi/filo/ui/theme/components/FileRow.kt
+++ app/src/main/java/io/mhdkhubbi/filo/ui/theme/components/FileRow.kt
@@ -6,31 +6,35 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Sort
 import androidx.compose.material.icons.automirrored.filled.ViewList
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.mhdkhubbi.filo.ui.theme.Gray400
 
 @Composable
-fun FloderViewMode(){
+fun FileRow(nameFile: String) {
 
-    Row(Modifier.fillMaxWidth()){
+    Row(Modifier.fillMaxWidth()) {
 
-        Icon(
-            Icons.AutoMirrored.Filled.Sort,
-            contentDescription = "Search",
-            tint = Gray400,
-            modifier = Modifier.padding(start = 15.dp).size(24.dp)
+        Text(
+            nameFile,
+            modifier = Modifier.padding(start = 10.dp, top = 10.dp),
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 15.sp,
         )
-         Spacer(Modifier.weight(1f))
+        Spacer(Modifier.weight(1f))
         Icon(
             Icons.AutoMirrored.Filled.ViewList,
             contentDescription = "Search",
             tint = Gray400,
-            modifier = Modifier.padding(end = 15.dp).size(24.dp)
+            modifier = Modifier
+                .padding(end = 15.dp)
+                .size(24.dp)
         )
     }
 
