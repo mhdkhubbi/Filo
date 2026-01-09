f73d729f31dfd249faf8a757af7eae5cdc75c8aa
mhdkhubbi <mhd.khubbi@gmail.com>
2026-01-09 15:29:53 +0300

    updating FileScreen.kt with Folder Creation UI and Scaffold integration

--- app/src/main/java/io/mhdkhubbi/filo/ui/theme/screens/FileScreen.kt
+++ app/src/main/java/io/mhdkhubbi/filo/ui/theme/screens/FileScreen.kt
@@ -3,23 +3,39 @@ package io.mhdkhubbi.filo.ui.theme.screens
 import FileScreenViewModel
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddBox
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
+import io.mhdkhubbi.filo.ui.theme.Gray100
+import io.mhdkhubbi.filo.ui.theme.Gray500
 import io.mhdkhubbi.filo.ui.theme.components.FileList
 import io.mhdkhubbi.filo.ui.theme.components.FileRow
 import io.mhdkhubbi.filo.ui.theme.components.TopBar
@@ -52,106 +68,192 @@ fun FileScreen(
             viewModel.clearNavigationResetFlag()
         }
     }
-    Column {
-        TopBar()
-        Spacer(Modifier.height(20.dp))
+    Scaffold(floatingActionButton =  {
+        ExtendedFloatingActionButton(onClick = {
+            viewModel.createFolderDialog=true
+        }) { Text("Add Folder") }
 
-        if (viewModel.pendingOperation != null && isInFileScreen) {
-            Row {
-                Button(
-                    onClick = {
-                        viewModel.executePendingOperation()
+
+    }) {
+        Column {
+
+
+
+            TopBar()
+            Spacer(Modifier.height(20.dp))
+
+            if (viewModel.pendingOperation != null && isInFileScreen) {
+                Row {
+                    Button(
+                        onClick = {
+                            viewModel.executePendingOperation()
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    ) {
+                        Text("Paste here")
+                    }
+                }
+            }
+            if (viewModel.isProcessing) {
+                AlertDialog(
+                    onDismissRequest = { },
+                    confirmButton = {},
+                    dismissButton = {},
+                    title = { Text("Processing") },
+                    text = {
+                        LinearProgressIndicator(
+                            progress = { viewModel.progress.coerceIn(0.01f, 1f) },
+                            modifier = Modifier.fillMaxWidth().height(6.dp)
+                        )
+                    }
+                )
+            }
+            if(viewModel.createFolderDialog){
+                AlertDialog(
+                    onDismissRequest = { viewModel.createFolderDialog = false },
+
+                    confirmButton = {
+                        TextButton(onClick = {
+                            viewModel.createFolderDialog = false
+                            viewModel.addingFolder()
+
+                        }) {
+                            Text("Add")
+                        }
                     },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(10.dp)
-                ) {
-                    Text("Paste here")
-                }
+
+                    dismissButton = {
+                        TextButton(onClick = {
+                            viewModel.createFolderDialog = false
+                        }) {
+                            Text("Cancel")
+
+                        }
+                    },
+
+                    title = { Text("Adding Folder") },
+                    text = {
+                        FolderField(viewModel.folderNameToAdd,
+                            {viewModel.folderNameChange(it)})
+                    }
+                )
             }
-        }
-        if (viewModel.isProcessing) {
-            AlertDialog(
-                onDismissRequest = { },
-                confirmButton = {},
-                dismissButton = {},
-                title = { Text("Processing") },
-                text = {
-                    LinearProgressIndicator(
-                        progress = { viewModel.progress.coerceIn(0.01f, 1f) },
-                        modifier = Modifier.fillMaxWidth().height(6.dp)
-                    )
+            if (viewModel.showDialog) {
+                AlertDialog(
+                    onDismissRequest = { viewModel.showDialog = false },
+
+                    confirmButton = {
+                        TextButton(onClick = {
+                            viewModel.showDialog = false
+                            viewModel.deleteSelected()
+                        }) {
+                            Text("Delete")
+                        }
+                    },
+
+                    dismissButton = {
+                        TextButton(onClick = {
+                            viewModel.showDialog = false
+                        }) {
+                            Text("Cancel")
+
+                        }
+                    },
+
+                    title = { Text("Delete selected items?") },
+                    text = { Text("This action cannot be undone.") }
+                )
+            }
+            FileRow(
+                directoryFile = viewModel.fileName,
+                isShown = viewModel.isShown,
+                onSelectAll = { viewModel.selectAll(viewModel.files) },
+                onDeselectAll = { viewModel.clearSelection() },
+                onCopy = {
+                    viewModel.onCopy()
+                    onNavigation(HomeScreen)
+
+                },
+                onMove = {
+                    viewModel.onMove()
+                    onNavigation(HomeScreen)
+                },
+                onDelete = {
+                    viewModel.showDialog = true
                 }
             )
-        }
 
-        if (viewModel.showDialog) {
-            AlertDialog(
-                onDismissRequest = { viewModel.showDialog = false },
-
-                confirmButton = {
-                    TextButton(onClick = {
-                        viewModel.showDialog = false
-                        viewModel.deleteSelected()
-                    }) {
-                        Text("Delete")
-                    }
+            FileList(
+                onNavigation,
+                files = viewModel.files,
+                isLoading = viewModel.isLoading,
+                selectedPaths = viewModel.selectedPaths,
+                toggleSelection = { path -> viewModel.toggleSelection(path) },
+                clearSelection = { viewModel.clearSelection() },
+                onCopy = {
+                    viewModel.onCopyItem(it)
+                    onNavigation(HomeScreen)
                 },
-
-                dismissButton = {
-                    TextButton(onClick = {
-                        viewModel.showDialog = false
-                    }) {
-                        Text("Cancel")
-
-                    }
+                onMove = {
+                    viewModel.onMoveItem(it)
+                    onNavigation(HomeScreen)
                 },
+                onDelete = {
+                    viewModel.deleteOne(it)
+                }
 
-                title = { Text("Delete selected items?") },
-                text = { Text("This action cannot be undone.") }
             )
+
         }
-        FileRow(
-            directoryFile = viewModel.fileName,
-            isShown = viewModel.isShown,
-            onSelectAll = { viewModel.selectAll(viewModel.files) },
-            onDeselectAll = { viewModel.clearSelection() },
-            onCopy = {
-                viewModel.onCopy()
-                onNavigation(HomeScreen)
-
-            },
-            onMove = {
-                viewModel.onMove()
-                onNavigation(HomeScreen)
-            },
-            onDelete = {
-                viewModel.showDialog = true
-            }
-        )
-
-        FileList(
-            onNavigation,
-            files = viewModel.files,
-            isLoading = viewModel.isLoading,
-            selectedPaths = viewModel.selectedPaths,
-            toggleSelection = { path -> viewModel.toggleSelection(path) },
-            clearSelection = {viewModel.clearSelection()},
-            onCopy = {viewModel.onCopyItem(it)
-                onNavigation(HomeScreen)
-                     },
-            onMove = {viewModel.onMoveItem(it)
-                onNavigation(HomeScreen)
-                     },
-            onDelete = {
-                viewModel.deleteOne(it)
-            }
-
-        )
-
     }
 
 
+}
+@Composable
+fun FolderField(
+    nameFolder: String="",
+    onValueChange: (String) -> Unit={}
+) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 10.dp, end = 10.dp)
+            .height(40.dp)
+            .background(Gray100, RoundedCornerShape(12.dp)),
+        contentAlignment = Alignment.Center
+    ) {
+        Row( modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                Icons.Default.AddBox,
+                contentDescription = "Search",
+                tint = Gray500
+            )
+
+            Spacer(Modifier.width(8.dp))
+
+            BasicTextField(
+                value = nameFolder,
+                onValueChange = {onValueChange(it)},
+                singleLine = true,
+                textStyle = LocalTextStyle.current.copy(color = Color.Black),
+                decorationBox = { innerTextField ->
+                    if (nameFolder.isEmpty()) {
+                        Text(
+                            "Add folder",
+                            color = Gray500
+                        )
+                    }
+                    innerTextField()
+                }
+            )
+        }
+    }
 }
 
 
