0c5f75999d93130acf13c0fe5574f4374982b56b
mhdkhubbi <mhd.khubbi@gmail.com>
2026-01-12 13:28:29 +0300

    updating MainActivity to force LTR and replacing AppRoot with MainScreen

--- app/src/main/java/io/mhdkhubbi/filo/MainActivity.kt
+++ app/src/main/java/io/mhdkhubbi/filo/MainActivity.kt
@@ -13,54 +13,73 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.net.toUri
 import io.mhdkhubbi.filo.ui.theme.FiloTheme
-import io.mhdkhubbi.filo.ui.theme.screens.AppRoot
+import io.mhdkhubbi.filo.ui.theme.screens.MainScreen
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.R)
+    override fun attachBaseContext(newBase: android.content.Context) {
+        val configuration = android.content.res.Configuration(newBase.resources.configuration)
+        // Force LTR direction
+        configuration.setLayoutDirection(java.util.Locale.US)
+        val context = newBase.createConfigurationContext(configuration)
+        super.attachBaseContext(context)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        if (!Environment.isExternalStorageManager()) {
-            val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION).apply {
-                data = "package:${applicationContext.packageName}".toUri()
-            }
-            startActivity(intent)
-        }
 
+        enableEdgeToEdge()
         setContent {
             FiloTheme {
 
-                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                        AppRoot(
-                            Modifier.padding(innerPadding),
+                var showUI by remember { mutableStateOf(false) }
+                LaunchedEffect(Unit) {
+                    delay(50) // Tiny delay to allow the Splash to settle
+                    showUI = true
+                }
 
-                        )
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    if (showUI) {
+                        MainScreen(Modifier.padding(innerPadding))
                     }
-
+                }
+                LaunchedEffect(Unit) {
+                    withContext(Dispatchers.IO) {
+                        if (!Environment.isExternalStorageManager()) {
+                            val intent =
+                                Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION).apply {
+                                    data = "package:$packageName".toUri()
+                                }
+                            startActivity(intent)
+                        }
+                    }
+                }
             }
         }
     }
 }
 
 
-
-
-
-
-
-
 @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+private fun GreetingPreview() {
     FiloTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            AppRoot(Modifier.padding(innerPadding))
+            MainScreen(Modifier.padding(innerPadding))
         }
     }
 }
