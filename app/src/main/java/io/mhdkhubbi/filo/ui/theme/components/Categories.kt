fe13ee7f0000ed2ef7d1a555cfde3228ad6cbc26
mhdkhubbi <mhd.khubbi@gmail.com>
2026-01-12 21:48:54 +0300

    updating theme colors and color schemes

--- app/src/main/java/io/mhdkhubbi/filo/ui/theme/components/Categories.kt
+++ app/src/main/java/io/mhdkhubbi/filo/ui/theme/components/Categories.kt
@@ -1,5 +1,6 @@
 package io.mhdkhubbi.filo.ui.theme.components
 
+import Gray100
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -11,40 +12,37 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AudioFile
-import androidx.compose.material.icons.filled.Download
-import androidx.compose.material.icons.filled.FilePresent
-import androidx.compose.material.icons.filled.Photo
-import androidx.compose.material.icons.filled.VideoLabel
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation3.runtime.NavKey
-import io.mhdkhubbi.filo.ui.theme.Gray100
-
+import io.mhdkhubbi.filo.R
+import io.mhdkhubbi.filo.domain.MediaType
+import io.mhdkhubbi.filo.ui.theme.screens.FileScreen
 
 @Composable
-fun Categories(onNavigation:(NavKey)->Unit, modifier: Modifier = Modifier) {
-
-
+fun Categories(onNavigation:(NavKey)->Unit,
+     load:(MediaType)->Unit
+    ,modifier: Modifier = Modifier) {
     Column(
         Modifier
-            .padding(start = 10.dp, top = 15.dp)
+            .padding(start = 15.dp,)
             .fillMaxSize()
     )
     {
         Text(
-
             text = "Categories",
             fontWeight = FontWeight.SemiBold,
-            fontSize = 15.sp,
+            fontSize = 20.sp,
         )
         FlowRow(
             Modifier
@@ -54,51 +52,53 @@ fun Categories(onNavigation:(NavKey)->Unit, modifier: Modifier = Modifier) {
 
 
         ) {
-
             Box(
                 Modifier.padding(20.dp).size(100.dp).weight(1f).border(
                     width = 1.dp,
-                    color = Gray100,
+                    Gray100,
                     shape = RoundedCornerShape(12.dp)
                 ).clickable {
-              //      onNavigation(MediaScreen)
+                    onNavigation(FileScreen("media_images_root"))
+
                 },
                 contentAlignment = Alignment.Center) {
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Icon(
-                        Icons.Default.Photo, contentDescription = "photos",
-                        modifier = Modifier
-                            .size(52.dp)
+                        painter=painterResource(R.drawable.images), contentDescription = "photos",
+                        modifier = Modifier.size(52.dp),tint=Color.Unspecified,
+
                     )
                     Spacer(Modifier.height(10.dp))
                     Text(
 
                         text = "Photos",
                         fontWeight = FontWeight.Medium,
-                        fontSize = 15.sp,
+                        fontSize = 13.sp,
                     )
                 }
             }
 
             Box( Modifier.padding(20.dp).size(100.dp).weight(1f).border(
                 width = 1.dp,
-                color = Gray100,
+                Gray100,
                 shape = RoundedCornerShape(12.dp)
-            ).clickable {}, contentAlignment = Alignment.Center) {
+            ).clickable {
+                onNavigation(FileScreen("media_videos_root"))
+            }, contentAlignment = Alignment.Center) {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     Icon(
-                        Icons.Default.VideoLabel, contentDescription = "photos",
+                        painterResource(R.drawable.videos), contentDescription = "videos",
                         modifier = Modifier
-                            .size(52.dp)
+                            .size(52.dp),tint=Color.Unspecified,
                     )
                     Spacer(Modifier.height(10.dp))
                     Text(
 
                         text = "Videos",
                         fontWeight = FontWeight.Medium,
-                        fontSize = 15.sp,
+                        fontSize = 13.sp,
                     )
                 }
             }
@@ -107,22 +107,22 @@ fun Categories(onNavigation:(NavKey)->Unit, modifier: Modifier = Modifier) {
 
             Box( Modifier.padding(20.dp).size(100.dp).weight(1f).border(
                 width = 1.dp,
-                color = Gray100,
+                Gray100,
                 shape = RoundedCornerShape(12.dp)
-            ).clickable {}, contentAlignment = Alignment.Center)
+            ).clickable {onNavigation(FileScreen("media_music_root"))}, contentAlignment = Alignment.Center)
             {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     Icon(
-                        Icons.Default.AudioFile, contentDescription = "photos",
+                        painterResource(R.drawable.music), contentDescription = "Music",
                         modifier = Modifier
-                            .size(52.dp)
+                            .size(52.dp),tint=Color.Unspecified,
                     )
                     Spacer(Modifier.height(10.dp))
                     Text(
 
                         text = "Music",
                         fontWeight = FontWeight.Medium,
-                        fontSize = 15.sp,
+                        fontSize = 13.sp,
                     )
                 }
             }
@@ -131,19 +131,19 @@ fun Categories(onNavigation:(NavKey)->Unit, modifier: Modifier = Modifier) {
                 width = 1.dp,
                 color = Gray100,
                 shape = RoundedCornerShape(12.dp)
-            ).clickable {}, contentAlignment = Alignment.Center) {
+            ).clickable {onNavigation(FileScreen("media_document_root"))}, contentAlignment = Alignment.Center) {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     Icon(
-                        Icons.Default.FilePresent, contentDescription = "photos",
+                        painterResource(R.drawable.documents), contentDescription = "Documents",
                         modifier = Modifier
-                            .size(52.dp)
+                            .size(52.dp),tint=Color.Unspecified,
                     )
                     Spacer(Modifier.height(10.dp))
                     Text(
 
                         text = "Documents",
                         fontWeight = FontWeight.Medium,
-                        fontSize = 15.sp,
+                        fontSize = 13.sp,
                     )
                 }
             }
@@ -154,22 +154,23 @@ fun Categories(onNavigation:(NavKey)->Unit, modifier: Modifier = Modifier) {
                 width = 1.dp,
                 color = Gray100,
                 shape = RoundedCornerShape(12.dp)
-            ).clickable {}, contentAlignment = Alignment.Center)
+            ).clickable {onNavigation(FileScreen("media_download_root"))}, contentAlignment = Alignment.Center)
             {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     Icon(
-                        Icons.Default.Download, contentDescription = "photos",
+                        painterResource(R.drawable.downloads), contentDescription = "Downloads",
                         modifier = Modifier
-                            .size(52.dp)
+                            .size(52.dp),tint=Color.Unspecified,
                     )
                     Spacer(Modifier.height(10.dp))
                     Text(
                         text = "Downloads",
                         fontWeight = FontWeight.Medium,
-                        fontSize = 15.sp,
+                        fontSize = 13.sp,
                     )
                 }
             }
+            Spacer(Modifier.width(15.dp).weight(1f))
 
 
         }
